0d7abcc6c193c41cc8f51fd7e86632e4995330ed
dawonss <uomap@naver.com>
2024-04-15 03:28:33 +0900

    update

---
+++ Desktop/project/wodmarket/src/main/java/com/example/wodmarket/WodmarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.wodmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WodmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WodmarketApplication.class, args);
+	}
+
+}
---
+++ Desktop/project/wodmarket/src/test/java/com/example/wodmarket/WodmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wodmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WodmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
